cb0a0405fe634dea1eb57f8a8ebca2561231c041
Ashima Shrivastava <ashima206@gmail.com>
2016-12-23 20:48:16 -0500

    added styling for intro section

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+
+function intropage(){
+	var reqdSection = particleground(document.getElementById("intro-section"),{
+    dotColor: '#f9b379',
+    lineColor: '#f9b379'
+		
+	});
+	
+
+}
+intropage();
